33e4b098037078a508516fc4c43cf5d5fcbdd74d
Ali Muksin <ali.muksin0510@gmail.com>
2026-08-12 22:45:46 +0700

    update: settings configuration base on the .env file and give an example of it in git repository

--- src/configs/settings.py
+++ src/configs/settings.py
@@ -1,12 +1,12 @@
 from __future__ import annotations
 from pathlib import Path
 from pydantic_settings import BaseSettings, SettingsConfigDict
-import os
 
 PROJECT_ROOT = Path(__file__).resolve().parents[2]
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
+        env_prefix="MANUAL_PARSER_",
         env_file=PROJECT_ROOT / ".env",
         env_file_encoding="utf-8",
         env_ignore_empty=True,

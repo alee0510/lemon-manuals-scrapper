5a9e363015fd16d8fd3f11c6b9c006896a94ccbf
Ali Muksin <ali.muksin0510@gmail.com>
2026-08-12 21:15:09 +0700

    feat: initialize project structure and add configuration with core extractor modules

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from web-scrapper!")
+from src.configs.settings import settings
 
+def main():
+    print(settings.data_dir)
+    print(settings.output_dir)
 
 if __name__ == "__main__":
     main()
